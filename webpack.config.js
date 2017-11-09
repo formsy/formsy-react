4caf2e6ac4cbd0797ba2a2fd093403c593b443d0
Andrea Vanini <andrea.vanini@uidu.org>
2017-11-10 00:27:32 +0100

    Loosen react dep allowing 16+, add reactDOM to externals in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,37 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  devtool: 'source-map',
-  entry: path.resolve(__dirname, 'src', 'index.js'),
-  externals: 'react',
+  devtool: "source-map",
+  entry: path.resolve(__dirname, "src", "index.js"),
   output: {
-    path: path.resolve(__dirname, 'release'),
-    filename: 'formsy-react.js',
-    libraryTarget: 'umd',
-    library: 'Formsy',
+    path: path.resolve(__dirname, "release"),
+    filename: "formsy-react.js",
+    libraryTarget: "umd",
+    library: "Formsy"
+  },
+  externals: {
+    react: {
+      root: "React",
+      commonjs2: "react",
+      commonjs: "react",
+      amd: "react",
+      umd: "react"
+    },
+    "react-dom": {
+      root: "ReactDOM",
+      commonjs2: "react-dom",
+      commonjs: "react-dom",
+      amd: "react-dom",
+      umd: "react-dom"
+    }
   },
   module: {
-    loaders: [{
-      test: /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: 'babel-loader',
-    }],
-  },
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
 };
