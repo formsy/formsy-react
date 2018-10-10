507eab7e0397039ab5115545f93845011992b6fc
Justin McMurdie <mcmurdiej@gmail.com>
2018-10-10 11:52:10 -0600

    fixing linting errors

--- src/Wrapper.js
+++ src/Wrapper.js
@@ -169,7 +169,7 @@ export default (Component) => {
     }
 
     registerRef = (refName, ref) => {
-      this[refName] = ref
+      this[refName] = ref;
     }
 
     showError = () => !this.showRequired() && !this.isValid();
