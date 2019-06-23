bfea207bb5dc98f186a1600d267097d5be95b5db
Robert Kuykendall <robert@rkuykendall.com>
2019-06-23 19:56:13 -0400

    Convert index / Formsy to Typescript

--- dist/formsy-react.umd.js
+++ dist/formsy-react.umd.js
@@ -159,53 +159,6 @@
     return _assertThisInitialized(self);
   }
 
-  function toObj(source) {
-    return Object.keys(source).reduce(function (output, key) {
-      var parentKey = key.match(/[^\[]*/i);
-      var paths = key.match(/\[.*?\]/g) || [];
-      paths = [parentKey[0]].concat(paths).map(function (key) {
-        return key.replace(/\[|\]/g, '');
-      });
-      var currentPath = output;
-      while (paths.length) {
-        var pathKey = paths.shift();
-
-        if (pathKey in currentPath) {
-          currentPath = currentPath[pathKey];
-        } else {
-          currentPath[pathKey] = paths.length ? isNaN(paths[0]) ? {} : [] : source[key];
-          currentPath = currentPath[pathKey];
-        }
-      }
-
-      return output;
-    }, {});
-  }
-
-  function fromObj(obj) {
-    function recur(newObj, propName, currVal) {
-      if (Array.isArray(currVal) || Object.prototype.toString.call(currVal) === '[object Object]') {
-        Object.keys(currVal).forEach(function(v) {
-          recur(newObj, propName + "[" + v + "]", currVal[v]);
-        });
-        return newObj;
-      }
-
-      newObj[propName] = currVal;
-      return newObj;
-    }
-
-    var keys = Object.keys(obj);
-    return keys.reduce(function(newObj, propName) {
-      return recur(newObj, propName, obj[propName]);
-    }, {});
-  }
-
-  var formDataToObject = {
-    fromObj: fromObj,
-    toObj: toObj
-  };
-
   function unwrapExports (x) {
   	return x && x.__esModule && Object.prototype.hasOwnProperty.call(x, 'default') ? x['default'] : x;
   }
@@ -1356,6 +1309,53 @@
   }
   });
 
+  function toObj(source) {
+    return Object.keys(source).reduce(function (output, key) {
+      var parentKey = key.match(/[^\[]*/i);
+      var paths = key.match(/\[.*?\]/g) || [];
+      paths = [parentKey[0]].concat(paths).map(function (key) {
+        return key.replace(/\[|\]/g, '');
+      });
+      var currentPath = output;
+      while (paths.length) {
+        var pathKey = paths.shift();
+
+        if (pathKey in currentPath) {
+          currentPath = currentPath[pathKey];
+        } else {
+          currentPath[pathKey] = paths.length ? isNaN(paths[0]) ? {} : [] : source[key];
+          currentPath = currentPath[pathKey];
+        }
+      }
+
+      return output;
+    }, {});
+  }
+
+  function fromObj(obj) {
+    function recur(newObj, propName, currVal) {
+      if (Array.isArray(currVal) || Object.prototype.toString.call(currVal) === '[object Object]') {
+        Object.keys(currVal).forEach(function(v) {
+          recur(newObj, propName + "[" + v + "]", currVal[v]);
+        });
+        return newObj;
+      }
+
+      newObj[propName] = currVal;
+      return newObj;
+    }
+
+    var keys = Object.keys(obj);
+    return keys.reduce(function(newObj, propName) {
+      return recur(newObj, propName, obj[propName]);
+    }, {});
+  }
+
+  var formDataToObject = {
+    fromObj: fromObj,
+    toObj: toObj
+  };
+
   var utils = {
     arraysDiffer: function arraysDiffer(a, b) {
       var _this = this;
@@ -1823,6 +1823,9 @@
       _classCallCheck(this, Formsy);
 
       _this = _possibleConstructorReturn(this, _getPrototypeOf(Formsy).call(this, props));
+      _this.inputs = void 0;
+      _this.emptyArray = void 0;
+      _this.prevInputNames = void 0;
 
       _this.getChildContext = function () {
         return {
