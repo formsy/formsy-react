76d8622263571295348f06135e3adc99170a8c25
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2026-05-18 11:00:55 +0300

    feat: export InjectedProps types

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { addValidationRule, validationRules } from './validationRules';
-import { withFormsy, PassDownProps } from './withFormsy';
+import { withFormsy, type PassDownProps, type InjectedProps } from './withFormsy';
 import { Formsy } from './Formsy';
 
 export { withFormsy };
-export type { PassDownProps as FormsyInjectedProps };
+export type { PassDownProps as FormsyInjectedProps, InjectedProps };
 export default Formsy;
